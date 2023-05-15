6298d2bb92511e631254c13555caa238679b1292
Joel Batista <2210996@my.ipleiria.pt>
2023-05-15 12:33:06 +0100

    initial commit

---
+++ warehouse/warehouse_problemforGA.py
@@ -0,0 +1,24 @@
+from ga.problem import Problem
+from warehouse.warehouse_agent_search import WarehouseAgentSearch
+from warehouse.warehouse_individual import WarehouseIndividual
+
+
+class WarehouseProblemGA(Problem):
+    def __init__(self, agent_search: WarehouseAgentSearch):
+        # TODO
+        self.forklifts = agent_search.forklifts
+        self.products = agent_search.products
+        self.agent_search = agent_search
+
+    def generate_individual(self) -> "WarehouseIndividual":
+        new_individual = WarehouseIndividual(self, len(self.agent_search.pairs))
+        #new_individual.initialize(self.prob1s)
+        return new_individual
+
+    def __str__(self):
+        string = "# of forklifts: "
+        string += f'{len(self.forklifts)}'
+        string = "# of products: "
+        string += f'{len(self.products)}'
+        return string
+
