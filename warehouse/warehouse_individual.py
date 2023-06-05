d13b59842397104440d08356ccce1b65c4e7f035
Joel Batista <2210996@my.ipleiria.pt>
2023-06-05 10:54:31 +0100

    compute_fitness() version 2 (fix needed)

--- warehouse/warehouse_individual.py
+++ warehouse/warehouse_individual.py
@@ -9,27 +9,61 @@ class WarehouseIndividual(IntVectorIndividual):
         super().__init__(problem, num_genes)
         self.total_distance = None
         self.max_steps = None
+        # TODO self.forklifts_path = None
 
 
     def compute_fitness(self) -> float:
         self.total_distance = 0
         self.max_steps = 0
-        forklift_index = 0
+
+        """
         # 1º forklift -> 1º indice genoma
-        pair = self.find_pair(self.problem.forklifts[forklift_index], self.problem.products[self.genome[0] - 1])
-        self.total_distance += pair.value
+        self.total_distance += self.get_pair_distance(self.problem.forklifts[forklift_index],
+                                                      self.problem.products[self.genome[0] - 1])
         # self.max_steps += pair.steps
-        i = 0
+
+        i = 0 # loop a percorrer o genoma
         while i < (len(self.genome) - 1):
+            # mudar de forklift
+            if self.genome[i] > len(self.problem.products):
+                forklift_index += 1
+                self.total_distance += self.get_pair_distance(self.problem.forklifts[forklift_index],
+                                                              self.problem.products[self.genome[i+1] - 1])
+                # self.max_steps += pair.steps
+            if self.genome[i+1] > len(self.problem.products):
+                self.total_distance += self.get_pair_distance(self.problem.products[self.genome[i] - 1],
+                                                              self.problem.forklifts[forklift_index])
             # genoma -> genoma, a comecar no primeiro indice
-            pair = self.find_pair(self.problem.products[self.genome[i] - 1], self.problem.products[self.genome[i+1] - 1])
-            self.total_distance += pair.value
+            self.total_distance += self.get_pair_distance(self.problem.products[self.genome[i] - 1],
+                                                          self.problem.products[self.genome[i+1] - 1])
             #self.max_steps += pair.steps
             i += 1
         # ultimo genoma -> saida
-        pair = self.find_pair(self.problem.products[self.genome[i] - 1], self.problem.agent_search.exit)
-        self.total_distance += pair.value
+        self.total_distance += self.get_pair_distance(self.problem.products[self.genome[i] - 1],
+                                                      self.problem.agent_search.exit)
         # self.max_steps += pair.steps
+        self.fitness = self.total_distance
+        return self.max_steps"""
+
+        genome_index = 0
+        for forklift in self.problem.forklifts:
+            if genome_index > len(self.genome) or self.genome[genome_index] > len(self.problem.products):
+                self.total_distance += self.get_pair_distance(forklift, self.problem.agent_search.exit)
+                genome_index += 1
+                continue
+
+            # forklift -> 1º do genoma
+            self.total_distance += self.get_pair_distance(forklift, self.problem.products[self.genome[genome_index] - 1])
+            genome_index += 1
+
+            # enquanto nao encontra outro forklift ou nao acabou genoma
+            while genome_index < len(self.genome) and self.genome[genome_index] <= len(self.problem.products):
+                self.total_distance += self.get_pair_distance(self.problem.products[self.genome[genome_index-1] - 1],
+                                                              self.problem.products[self.genome[genome_index] - 1])
+                genome_index += 1
+            # forklift -> exit
+            self.total_distance += self.get_pair_distance(forklift, self.problem.agent_search.exit)
+
         self.fitness = self.total_distance
         return self.max_steps
 
@@ -39,10 +73,10 @@ class WarehouseIndividual(IntVectorIndividual):
         # incluir posicao inicial forklift
         pass
 
-    def find_pair(self, cell1, cell2):
+    def get_pair_distance(self, cell1, cell2):
         for p in self.problem.agent_search.pairs:
             if (p.cell1 == cell1 and p.cell2 == cell2) or (p.cell1 == cell2 and p.cell2 == cell1):
-                return p
+                return p.value
         return None
 
     def __str__(self):
