5b9b06cc35e23a9e500bcd28a809cda48c2c0b4d
Joel Batista <joelbasim@gmail.com>
2023-06-25 20:41:12 +0000

    mutation3 (swap) e recombination3 (order crossover)

--- ga/genetic_operators/mutation3.py
+++ ga/genetic_operators/mutation3.py
@@ -1,3 +1,4 @@
+from ga.genetic_algorithm import GeneticAlgorithm
 from ga.individual_int_vector import IntVectorIndividual
 from ga.genetic_operators.mutation import Mutation
 
@@ -6,8 +7,24 @@ class Mutation3(Mutation):
         super().__init__(probability)
 
     def mutate(self, ind: IntVectorIndividual) -> None:
-        # TODO
-        pass
+        """
+        Mutation: pick 2 random values from genome and swap them
+        """
+
+        # pick random indexes
+        index1 = 0
+        index2 = 0
+        length = len(ind.genome)
+        while index1 == index2:
+            index1 = GeneticAlgorithm.rand.randint(1, length - 1)
+            index2 = GeneticAlgorithm.rand.randint(1, length - 1)
+
+        # adjust indexes
+        if index1 > index2:
+            index1, index2 = index2, index1
+
+        # swap values
+        ind.genome[index1], ind.genome[index2] = ind.genome[index2], ind.genome[index1]
 
     def __str__(self):
         return "Mutation 3 (" + f'{self.probability}' + ")"
--- ga/genetic_operators/recombination3.py
+++ ga/genetic_operators/recombination3.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+from ga.genetic_algorithm import GeneticAlgorithm
 from ga.genetic_operators.recombination import Recombination
 from ga.individual import Individual
 
@@ -7,8 +10,28 @@ class Recombination3(Recombination):
         super().__init__(probability)
 
     def recombine(self, ind1: Individual, ind2: Individual) -> None:
-        # TODO
-        pass
+        parent1 = ind1.genome
+        parent2 = ind2.genome
+
+        ind1.genome = self.ox(parent1, parent2)
+        ind2.genome = self.ox(parent2, parent1)
+
+    def ox(self, parent1, parent2):
+        length = len(parent1)
+        # choose random indexes
+        index1 = GeneticAlgorithm.rand.randint(1, length - 1)
+        index2 = GeneticAlgorithm.rand.randint(index1 + 1, length)
+
+        child = [-1] * length
+        child[index1:index2+1] = parent1[index1:index2+1]
+
+        for i in range(length):
+            if child[i] == -1:
+                for value in parent2:
+                    if value not in child:
+                        child[i] = value
+                        break
+        return child
 
     def __str__(self):
         return "Recombination 3 (" + f'{self.probability}' + ")"
